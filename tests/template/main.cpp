4fa50fe61ed38cf6f06bcb3cd634313a08d8428a
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-03-01 13:12:59 +0000

    Add basic template test

---
+++ tests/template/main.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+
+template <class T>
+class Ciao {
+  private:
+    T val;
+
+  public: 
+    void set(T v) {
+      val = v;
+    }
+
+    T get() {
+      return val;
+    }
+
+    T* get2() {
+      return &val; 
+    }
+};
+
+int main() {
+  Ciao<wstring> cW;
+  Ciao<string> cS;
+
+  cS.set("marco");
+  cW.get();
+  cS.get2();
+  cW.get2();
+}
