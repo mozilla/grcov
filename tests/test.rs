3b3237ab9bbbee5a76992df87fdcfe97e632d0f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-03-03 10:51:56 +0100

    Check equality after parsing JSON instead of string equality; don't check equality of modules as their names can be different on different machines

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,12 @@
 extern crate walkdir;
+extern crate serde_json;
 
 use std::process::Command;
 use walkdir::WalkDir;
 use std::path::Path;
 use std::fs::File;
 use std::io::Read;
+use serde_json::Value;
 
 fn make(path: &Path) {
     let status = Command::new("make")
@@ -56,6 +58,47 @@ fn make_clean(path: &Path) {
     assert!(status.success());
 }
 
+fn check_equal(expected_output: Vec<String>, output: Vec<String>) {
+    println!("Expected:");
+    for line in expected_output.iter() {
+        println!("{}", line);
+    }
+    let mut expected: Vec<Value> = Vec::new();
+    for line in expected_output.iter() {
+        expected.push(serde_json::from_str(line).unwrap());
+    }
+
+    println!("Got:");
+    for line in output.iter() {
+        println!("{}", line);
+    }
+    let mut actual: Vec<Value> = Vec::new();
+    for line in output.iter() {
+        actual.push(serde_json::from_str(line).unwrap());
+    }
+
+    for out in actual.iter() {
+        let exp = expected.iter().find(|&&ref x| x["sourceFile"] == out["sourceFile"]);
+        assert!(exp.is_some(), "Got unexpected {}", out["sourceFile"]);
+        let exp_val = exp.unwrap();
+        assert_eq!(out["testUrl"], exp_val["testUrl"]);
+        assert_eq!(out["covered"], exp_val["covered"]);
+        assert_eq!(out["uncovered"], exp_val["uncovered"]);
+        assert_eq!(out["methods"].as_object().unwrap().len(), exp_val["methods"].as_object().unwrap().len());
+    }
+
+    for exp in expected.iter() {
+        let out = actual.iter().find(|&&ref x| x["sourceFile"] == exp["sourceFile"]);
+        assert!(out.is_some(), "Missing {}", exp["sourceFile"]);
+        assert!(out.is_some());
+        let out_val = out.unwrap();
+        assert_eq!(exp["testUrl"], out_val["testUrl"]);
+        assert_eq!(exp["covered"], out_val["covered"]);
+        assert_eq!(exp["uncovered"], out_val["uncovered"]);
+        assert_eq!(exp["methods"].as_object().unwrap().len(), out_val["methods"].as_object().unwrap().len());
+    }
+}
+
 #[test]
 fn test_integration() {
     for entry in WalkDir::new("tests").min_depth(1) {
@@ -65,25 +108,7 @@ fn test_integration() {
             make(path);
             run(path);
 
-            let expected_output = read_expected(path);
-            println!("Expected:");
-            for line in expected_output.iter() {
-                println!("{}", line);
-            }
-
-            let output = run_grcov(path);
-            println!("Got:");
-            for line in output.iter() {
-                println!("{}", line);
-            }
-
-            for line in output.iter() {
-                assert_eq!(expected_output.iter().find(|&&ref x| x == line), Some(line), "Unexpected result present for {}", path.display());
-            }
-
-            for line in expected_output.iter() {
-                assert_eq!(output.iter().find(|&&ref x| x == line), Some(line), "Expected result not present for {}", path.display());
-            }
+            check_equal(read_expected(path), run_grcov(path));
 
             make_clean(path);
         }
