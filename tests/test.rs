004af089b66a6f1903f08fecd5972adc591cec99
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-09-11 13:13:26 +0200

    Test coveralls format

--- tests/test.rs
+++ tests/test.rs
@@ -26,23 +26,29 @@ fn run(path: &Path) {
     assert!(status.success());
 }
 
-fn read_expected(path: &Path, compiler: &str) -> Vec<String> {
-    let name = format!("expected_{}.txt", compiler);
+fn read_expected(path: &Path, compiler: &str, format: &str) -> String {
+    let name = format!("expected_{}.{}", compiler, format);
     let mut f = File::open(path.join(&name)).expect(format!("{} file not found", name).as_str());
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
-    let mut v = Vec::new();
-    for line in s.lines() {
-        v.push(line.to_string());
-    }
-    v
+    s
 }
 
-fn run_grcov(path: &Path, llvm: bool) -> Vec<String> {
+fn run_grcov(path: &Path, llvm: bool, output_format: &str) -> String {
     let mut args: Vec<String> = Vec::new();
+    args.push("--".to_string());
     if llvm {
-        args.push("--".to_string());
         args.push("--llvm".to_string());
+    }
+    args.push("-t".to_string());
+    args.push(output_format.to_string());
+    if output_format == "coveralls" {
+        args.push("--token".to_string());
+        args.push("TOKEN".to_string());
+        args.push("--commit-sha".to_string());
+        args.push("COMMIT".to_string());
+        args.push("-s".to_string());
+        args.push(path.to_str().unwrap().to_string());
     }
 
     let output = Command::new("cargo")
@@ -52,11 +58,7 @@ fn run_grcov(path: &Path, llvm: bool) -> Vec<String> {
                          .output()
                          .expect("Failed to run grcov");
     let s = String::from_utf8(output.stdout).unwrap();
-    let mut v = Vec::new();
-    for line in s.lines() {
-        v.push(line.to_string());
-    }
-    v
+    s
 }
 
 fn make_clean(path: &Path) {
@@ -85,14 +87,14 @@ fn check_equal_inner(a: &Value, b: &Value, skip_methods: bool) -> bool {
     a["file"]["total_uncovered"] == b["file"]["total_uncovered"]
 }
 
-fn check_equal(expected_output: &[String], output: &[String]) {
+fn check_equal_ade(expected_output: &String, output: &String) {
     let mut expected: Vec<Value> = Vec::new();
-    for line in expected_output {
+    for line in expected_output.lines() {
         expected.push(serde_json::from_str(line).unwrap());
     }
 
     let mut actual: Vec<Value> = Vec::new();
-    for line in output {
+    for line in output.lines() {
         actual.push(serde_json::from_str(line).unwrap());
     }
 
@@ -115,7 +117,50 @@ fn check_equal(expected_output: &[String], output: &[String]) {
         assert!(out.is_some(), "Missing {} - Full output: {:?}", exp, output);
     }
 
-    assert_eq!(expected.len(), actual_len, "Got same number of expected records.")
+    assert_eq!(expected.len(), actual_len, "Got same number of expected records.");
+}
+
+fn check_equal_coveralls(expected_output: &String, output: &String) {
+    let expected: Value = serde_json::from_str(expected_output).unwrap();
+    let actual: Value = serde_json::from_str(output).unwrap();
+
+    assert_eq!(expected["git"]["branch"], actual["git"]["branch"]);
+    assert_eq!(expected["git"]["head"]["id"], actual["git"]["head"]["id"]);
+    assert_eq!(expected["repo_token"], actual["repo_token"]);
+    assert_eq!(expected["service_job_number"], actual["service_job_number"]);
+    assert_eq!(expected["service_name"], actual["service_name"]);
+    assert_eq!(expected["service_number"], actual["service_number"]);
+
+    let mut actual_len = 0;
+
+    let actual_source_files = actual["source_files"].as_array().unwrap();
+    let expected_source_files = expected["source_files"].as_array().unwrap();
+
+    for out in actual_source_files {
+        let exp = expected_source_files.iter().find(|&&ref x| x["name"] == out["name"]);
+        assert!(exp.is_some(), "Got unexpected {} - Expected output: {:?}", out, expected_output);
+
+        let exp = exp.unwrap();
+
+        assert_eq!(exp["name"], out["name"]);
+        assert_eq!(exp["source_digest"], out["source_digest"], "Got wrong digest for {}", exp["name"]);
+        assert_eq!(exp["coverage"], out["coverage"], "Got wrong coverage for {}", exp["name"]);
+
+        actual_len += 1;
+    }
+
+    for exp in expected_source_files {
+        let out = actual_source_files.iter().find(|&&ref x| x["name"] == exp["name"]);
+        assert!(out.is_some(), "Missing {} - Full output: {:?}", exp, output);
+
+        let out = out.unwrap();
+
+        assert_eq!(out["name"], exp["name"]);
+        assert_eq!(out["source_digest"], exp["source_digest"], "Got wrong digest for {}", out["name"]);
+        assert_eq!(out["coverage"], exp["coverage"], "Got wrong coverage for {}", out["name"]);
+    }
+
+    assert_eq!(expected_source_files.len(), actual_len, "Got same number of source files.");
 }
 
 #[test]
@@ -131,7 +176,8 @@ fn test_integration() {
             println!("GCC");
             make(path, "g++");
             run(path);
-            check_equal(&read_expected(path, "gcc"), &run_grcov(path, false));
+            check_equal_ade(&read_expected(path, "gcc", "ade"), &run_grcov(path, false, "ade"));
+            check_equal_coveralls(&read_expected(path, "gcc", "coveralls"), &run_grcov(path, false, "coveralls"));
             make_clean(path);
 
             // On CI, don't test llvm, as there are problems for now.
@@ -141,7 +187,8 @@ fn test_integration() {
             make(path, "clang++");
             run(path);
             if !skip_llvm {
-                check_equal(&read_expected(path, "llvm"), &run_grcov(path, true));
+                check_equal_ade(&read_expected(path, "llvm", "ade"), &run_grcov(path, true, "ade"));
+                check_equal_coveralls(&read_expected(path, "llvm", "coveralls"), &run_grcov(path, true, "coveralls"));
             }
             make_clean(path);
         }
