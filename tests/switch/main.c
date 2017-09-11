de8f95d6ec2f9cf59002c1d440abfd2a4bce50b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-09-11 14:27:08 +0200

    Add test with 'switch' statement

---
+++ tests/switch/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+  for (int i = 1; i < 10; i++) {
+      switch (i) {
+          case 1:
+              printf("1");
+              break;
+          case 2:
+              printf("2");
+              break;
+          case 3:
+              printf("3");
+              break;
+          case 4:
+              printf("4");
+              break;
+          case 5:
+              printf("5");
+              break;
+          case 6:
+              printf("6");
+              break;
+          case 8:
+              printf("8");
+              break;
+          case 9:
+              printf("9");
+              break;
+          case 10:
+              printf("10");
+              break;
+      }
+  }
+
+  return 0;
+}
