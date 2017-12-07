9598f515cc15cf9c0d61d543368cad7bc78d8b82
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-12-07 17:26:49 +0100

    Add benchmarks for parse_lcov and parse_old_gcov too

--- benches/parser.rs
+++ benches/parser.rs
@@ -2,8 +2,25 @@
 extern crate test;
 extern crate grcov;
 
-use test::{black_box, Bencher};
 use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use test::{black_box, Bencher};
+
+#[bench]
+fn bench_parser_lcov(b: &mut Bencher) {
+    let f = File::open("./test/prova.info").expect("Failed to open lcov file");
+    b.iter(|| {
+        let file = BufReader::new(&f);
+        black_box(grcov::parse_lcov(file, true));
+    });
+}
+
+#[bench]
+fn bench_parser_old_gcov(b: &mut Bencher) {
+    let path = Path::new("./test/old_branches.gcov");
+    b.iter(|| black_box(grcov::parse_old_gcov(path, true)));
+}
 
 #[bench]
 fn bench_parser_gcov(b: &mut Bencher) {
