c7fa09a5c4c007e818537fc2ce6a1315cbab6f7c
Calixte Denizet <calixte.denizet@gmail.com>
2018-10-03 15:54:41 +0200

    Remove gcc for mac and build with clang everywhere

--- tests/test.rs
+++ tests/test.rs
@@ -433,7 +433,7 @@ fn test_integration() {
 
             do_clean(path);
 
-            if !cfg!(windows) {
+            if !cfg!(windows) && !cfg!(macos) {
                 println!("GCC");
                 let gpp = &get_tool("CXX", "g++");
                 let gcc_version = get_version(gpp);
@@ -484,7 +484,7 @@ fn test_integration_zip_zip() {
     for compiler in compilers {
         let is_llvm = compiler.contains("clang");
 
-        if cfg!(windows) && !is_llvm {
+        if (cfg!(windows) || cfg!(macos)) && !is_llvm {
             continue;
         }
 
@@ -569,7 +569,7 @@ fn test_integration_zip_dir() {
     for compiler in compilers {
         let is_llvm = compiler.contains("clang");
 
-        if cfg!(windows) && !is_llvm {
+        if (cfg!(windows) || cfg!(macos)) && !is_llvm {
             continue;
         }
 
