5d6715d3779ec43a21f1568fc945c31684b2831f
Marco <mcastelluccio@mozilla.com>
2019-01-09 17:35:02 +0100

    Be more verbose when gcov fails (#233)
    * Be more verbose when gcov fails
    
    * Remove unneeded commented-out code
    
    * Improved wording

--- src/gcov.rs
+++ src/gcov.rs
@@ -1,38 +1,7 @@
 use semver::Version;
 use std::env;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
-
-/*
-#[link(name = "gcov")]
-extern {
-    fn __gcov_read_unsigned() -> u32;
-    fn __gcov_open(name: *const c_char) -> i32;
-    fn __gcov_close();
-}
-
-fn gcov_open(file: String) -> i32 {
-    let c_to_print = CString::new(file).unwrap();
-    return unsafe { __gcov_open(c_to_print.as_ptr()) };
-}
-
-fn gcov_read_unsigned() -> u32 {
-    return unsafe { __gcov_read_unsigned() };
-}
-
-fn prova() {
-  if gcov_open("/home/marco/Documenti/workspace/grcov/tests/llvm/main.gcda".to_string()) == 1 {
-    println!("2");
-  }
-
-  println!("{:x}", gcov_read_unsigned());
-
-  if gcov_open("/home/marco/Documenti/workspace/grcov/tests/basic/main.gcda".to_string()) == 1 {
-    println!("1");
-  }
-
-  println!("{:x}", gcov_read_unsigned());
-}*/
+use std::process::Command;
 
 fn get_gcov() -> String {
     match env::var("GCOV") {
@@ -51,21 +20,16 @@ pub fn run_gcov(gcno_path: &PathBuf, branch_enabled: bool, working_dir: &PathBuf
     let status = command
         .arg(gcno_path)
         .arg("-i") // Generate intermediate gcov format, faster to parse.
-        .current_dir(working_dir)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null());
+        .current_dir(working_dir);
 
-    /*if cfg!(unix) {
-        status.spawn()
-              .expect("Failed to execute gcov process");
-    } else {*/
-    let status = status.status().expect("Failed to execute gcov process");
-    assert!(
-        status.success(),
-        "gcov wasn't successfully executed on {}",
-        gcno_path.display()
-    );
-    //}
+    let output = status.output().expect("Failed to execute gcov process");
+
+    if !output.status.success() {
+        eprintln!("gcov stdout: {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("gcov stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+        panic!("gcov execution failed on {}", gcno_path.display());
+    }
 }
 
 fn is_recent_version(gcov_output: &str) -> bool {
