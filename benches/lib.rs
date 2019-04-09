ab83ee06f30a82401e76f2d52f0804058799f6db
lawrencek0 <l.khadka@outlook.com>
2019-04-09 16:41:33 -0500

    move variables out of iter

--- benches/lib.rs
+++ benches/lib.rs
@@ -31,43 +31,42 @@ fn bench_lib_merge_results(b: &mut Bencher) {
             (2, vec![false, true]),
             (4, vec![true]),
         ]
-            .iter()
-            .cloned()
-            .collect(),
+        .iter()
+        .cloned()
+        .collect(),
         functions: functions1,
     };
 
-    b.iter(|| {
-        let mut functions2: FxHashMap<String, Function> = FxHashMap::default();
-        functions2.insert(
-            "f1".to_string(),
-            Function {
-                start: 1,
-                executed: false,
-            },
-        );
-        functions2.insert(
-            "f2".to_string(),
-            Function {
-                start: 2,
-                executed: true,
-            },
-        );
-        let result2 = CovResult {
-            lines: [(1, 21), (3, 42), (4, 7), (2, 0), (8, 0)]
-                .iter()
-                .cloned()
-                .collect(),
-            branches: [
-                (1, vec![false, false]),
-                (2, vec![false, true]),
-                (3, vec![true]),
-            ]
-                .iter()
-                .cloned()
-                .collect(),
-            functions: functions2,
-        };
-        black_box(grcov::merge_results(&mut result, result2));
-    });
+    let mut functions2: FxHashMap<String, Function> = FxHashMap::default();
+    functions2.insert(
+        "f1".to_string(),
+        Function {
+            start: 1,
+            executed: false,
+        },
+    );
+    functions2.insert(
+        "f2".to_string(),
+        Function {
+            start: 2,
+            executed: true,
+        },
+    );
+    let result2 = CovResult {
+        lines: [(1, 21), (3, 42), (4, 7), (2, 0), (8, 0)]
+            .iter()
+            .cloned()
+            .collect(),
+        branches: [
+            (1, vec![false, false]),
+            (2, vec![false, true]),
+            (3, vec![true]),
+        ]
+        .iter()
+        .cloned()
+        .collect(),
+        functions: functions2,
+    };
+
+    b.iter(|| black_box(grcov::merge_results(&mut result, result2.clone())));
 }
