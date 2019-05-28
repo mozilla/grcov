e7babade5894727cfaadee7b379dc726a15350e0
calixteman <calixte.denizet@gmail.com>
2019-05-28 15:51:07 +0200

    Normalize paths when canonicalization failed to do it (#290)
    * Normalize paths when canonicalization failed to do it
    
    * Skip file on normalization failures and print a warning on stderr
    
    * Unindent code

--- src/path_rewriting.rs
+++ src/path_rewriting.rs
@@ -40,14 +40,36 @@ pub fn canonicalize_path<P: AsRef<Path>>(path: P) -> io::Result<PathBuf> {
     Ok(path)
 }
 
-fn normalize_path<P: AsRef<Path>>(path: P) -> PathBuf {
-    let mut new_path = PathBuf::from("");
-    for component in path.as_ref().components() {
-        if component != Component::CurDir {
-            new_path.push(component);
+
+pub fn normalize_path<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    // Copied from Cargo sources: https://github.com/rust-lang/cargo/blob/master/src/cargo/util/paths.rs#L65
+    let mut components = path.as_ref().components().peekable();
+    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+        components.next();
+        PathBuf::from(c.as_os_str())
+    } else {
+        PathBuf::new()
+    };
+
+    for component in components {
+        match component {
+            Component::Prefix(..) => unreachable!(),
+            Component::RootDir => {
+                ret.push(component.as_os_str());
+            }
+            Component::CurDir => {}
+            Component::ParentDir => {
+                if !ret.pop() {
+                    eprintln!("Warning: {:?} cannot be normalized because of \"..\", so skip it.", path.as_ref());
+                    return None;
+                }
+            }
+            Component::Normal(c) => {
+                ret.push(c);
+            }
         }
     }
-    new_path
+    Some(ret)
 }
 
 // Search the source file's path in the mapping.
@@ -107,7 +129,7 @@ fn get_abs_path(
     source_dir: &Option<PathBuf>,
     rel_path: PathBuf,
     cache: &mut Option<PathBuf>,
-) -> (PathBuf, PathBuf) {
+) -> Option<(PathBuf, PathBuf)> {
     let mut abs_path = if !rel_path.is_relative() {
         rel_path.clone()
     } else if let Some(ref source_dir) = source_dir {
@@ -132,10 +154,15 @@ fn get_abs_path(
     // Fixup the relative path, in case the absolute path was a symlink.
     let rel_path = fixup_rel_path(&source_dir, &abs_path, rel_path);
     
-    // Normalize the path in removing './' or '//'
+    // Normalize the path in removing './' or '//' or '..'
     let rel_path = normalize_path(rel_path);
+    let abs_path = normalize_path(abs_path);
 
-    (abs_path, rel_path)
+    if rel_path.is_none() || abs_path.is_none() {
+        None
+    } else {
+        Some((abs_path.unwrap(), rel_path.unwrap()))
+    }
 }
 
 fn check_extension(path: &PathBuf, e: &str) -> bool {
@@ -264,7 +291,12 @@ pub fn rewrite_paths(
         };
 
         // Get absolute path to the source file.
-        let (abs_path, rel_path) = get_abs_path(&source_dir, rel_path, &mut cache);
+        let paths = get_abs_path(&source_dir, rel_path, &mut cache);
+        if paths.is_none() {
+            return None;
+        }
+
+        let (abs_path, rel_path) = paths.unwrap();
 
         if to_ignore_globset.is_match(&rel_path) {
             return None;
@@ -1203,7 +1235,11 @@ mod tests {
 
     #[test]
     fn test_normalize_path() {
-        assert_eq!(normalize_path("./foo//bar"), PathBuf::from("foo/bar"));
-        assert_eq!(normalize_path("./foo/./bar/./oof/"), PathBuf::from("foo/bar/oof"));
+        assert_eq!(normalize_path("./foo/bar").unwrap(), PathBuf::from("foo/bar"));
+        assert_eq!(normalize_path("./foo//bar").unwrap(), PathBuf::from("foo/bar"));
+        assert_eq!(normalize_path("./foo/./bar/./oof/").unwrap(), PathBuf::from("foo/bar/oof"));
+        assert_eq!(normalize_path("./foo/../bar/./oof/").unwrap(), PathBuf::from("bar/oof"));
+        assert!(normalize_path("../bar/oof/").is_none());
+        assert!(normalize_path("bar/foo/../../../oof/").is_none());
     }
 }
