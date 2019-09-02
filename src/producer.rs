bc1aa0e9f451161f5c1da2c514b7301da89d08bb
Crawford Leeds <crawford@cocoa.me>
2019-09-02 15:55:17 -0600

    fix conversion from PathBuf to string on windows

--- src/producer.rs
+++ src/producer.rs
@@ -43,7 +43,7 @@ fn clean_path(path: &PathBuf) -> String {
 
 #[cfg(windows)]
 fn clean_path(path: &PathBuf) -> String {
-    path.replace("\\", "/")
+    path.to_str().unwrap().to_string().replace("\\", "/")
 }
 
 impl Archive {
