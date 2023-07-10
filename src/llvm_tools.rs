e71ae871bd2210b41c41dfea3a6c1a4522942c0e
l3nn0x <dragon83.super@gmail.com>
2023-07-10 23:58:40 +0200

    run fmt

--- src/llvm_tools.rs
+++ src/llvm_tools.rs
@@ -222,9 +222,15 @@ mod tests {
         assert_eq!(lcovs.len(), 0);
 
         #[cfg(unix)]
-        let binary_path = format!("{}/debug/rust-code-coverage-sample", std::env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string()));
+        let binary_path = format!(
+            "{}/debug/rust-code-coverage-sample",
+            std::env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string())
+        );
         #[cfg(windows)]
-        let binary_path = format!("{}/debug/rust-code-coverage-sample.exe", std::env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string()));
+        let binary_path = format!(
+            "{}/debug/rust-code-coverage-sample.exe",
+            std::env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string())
+        );
 
         let lcovs = profraws_to_lcov(
             &[tmp_path.join("default.profraw")],
