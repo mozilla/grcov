3b4784822f07f760cb52aa168bc3de4c90d988ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-25 16:58:45 +0200

    Drop cargo_binutils dependency as it hasn't been updated for a while

--- src/llvm_tools.rs
+++ src/llvm_tools.rs
@@ -1,7 +1,8 @@
-use cargo_binutils::Tool;
 use once_cell::sync::OnceCell;
 use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+use std::env;
 use std::env::consts::EXE_SUFFIX;
+use std::error::Error;
 use std::ffi::OsStr;
 use std::fs;
 use std::io::Write;
@@ -146,12 +147,37 @@ pub fn profraws_to_lcov(
     Ok(results)
 }
 
+// The sysroot and rustlib functions are coming from https://github.com/rust-embedded/cargo-binutils/blob/a417523fa990c258509696507d1ce05f85dedbc4/src/rustc.rs.
+fn sysroot() -> Result<String, Box<dyn Error>> {
+    let rustc = env::var_os("RUSTC").unwrap_or_else(|| "rustc".into());
+    let output = Command::new(rustc).arg("--print").arg("sysroot").output()?;
+    // Note: We must trim() to remove the `\n` from the end of stdout
+    Ok(String::from_utf8(output.stdout)?.trim().to_owned())
+}
+
+// See: https://github.com/rust-lang/rust/blob/564758c4c329e89722454dd2fbb35f1ac0b8b47c/src/bootstrap/dist.rs#L2334-L2341
+fn rustlib() -> Result<PathBuf, Box<dyn Error>> {
+    let sysroot = sysroot()?;
+    let mut pathbuf = PathBuf::from(sysroot);
+    pathbuf.push("lib");
+    pathbuf.push("rustlib");
+    pathbuf.push(rustc_version::version_meta()?.host); // TODO: Prevent calling rustc_version::version_meta() multiple times
+    pathbuf.push("bin");
+    Ok(pathbuf)
+}
+
+fn llvm_tool_path(name: &str) -> Result<PathBuf, Box<dyn Error>> {
+    let mut path = rustlib()?;
+    path.push(format!("llvm-{}{}", name, EXE_SUFFIX));
+    Ok(path)
+}
+
 fn get_profdata_path() -> Result<PathBuf, String> {
     let path = if let Some(mut path) = LLVM_PATH.get().cloned() {
         path.push(format!("llvm-profdata{}", EXE_SUFFIX));
         path
     } else {
-        Tool::Profdata.path().map_err(|x| x.to_string())?
+        llvm_tool_path("profdata").map_err(|x| x.to_string())?
     };
 
     if !path.exists() {
@@ -166,7 +192,7 @@ fn get_cov_path() -> Result<PathBuf, String> {
         path.push(format!("llvm-cov{}", EXE_SUFFIX));
         path
     } else {
-        Tool::Cov.path().map_err(|x| x.to_string())?
+        llvm_tool_path("cov").map_err(|x| x.to_string())?
     };
 
     if !path.exists() {
