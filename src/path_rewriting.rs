5f769bb75a4164f5add726d814cf2bfdae2b668e
Jason Gilanfarr <jason.gilanfarr@gmail.com>
2025-01-27 15:19:32 -0800

    Missed two test fixes

--- src/path_rewriting.rs
+++ src/path_rewriting.rs
@@ -1046,9 +1046,8 @@ mod tests {
 
     #[cfg(unix)]
     #[test]
-    fn test_rewrite_paths_rewrite_path_for_java_kotlin_and_rust() {
+    fn test_rewrite_paths_rewrite_path_for_kotlin_and_rust() {
         let mut result_map: CovResultMap = FxHashMap::default();
-        result_map.insert("java/main.java".to_string(), empty_result!());
         result_map.insert("kt/main.kt".to_string(), empty_result!());
         result_map.insert("main.rs".to_string(), empty_result!());
         
@@ -1064,12 +1063,6 @@ mod tests {
             Default::default(),
         );
         assert!(results.len() == 1);
-
-        let (abs_path, rel_path, result) = results.remove(0);
-        assert!(abs_path.is_absolute());
-        assert!(abs_path.ends_with("test/java/main.java"));
-        assert_eq!(rel_path, PathBuf::from("test/java/main.java"));
-        assert_eq!(result, empty_result!());
 
         let (abs_path, rel_path, result) = results.remove(0);
         assert!(abs_path.is_absolute());
